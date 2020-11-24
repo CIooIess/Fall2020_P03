8e3a16dc3f9c50acb686bbd989d02634626a1c33
Chase <chasekat742@gmail.com>
2020-11-23 18:49:54 -0600

    Level done, Camera done, it's all done??
    It's all done? Always has been.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,9 +12,6 @@ public class CameraController : MonoBehaviour
     [SerializeField] Camera _mainCamera;
     [SerializeField] Image _fadeBox;
 
-    float cameraHeight;
-    float cameraWidth;
-
     void Start()
     {
         Camera = this;
@@ -22,17 +19,17 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        cameraHeight = _mainCamera.orthographicSize * 2;
-        cameraWidth = cameraHeight * _mainCamera.aspect;
-
-        float maxY = Physics2D.Raycast(transform.position, Vector2.up, Mathf.Infinity, _bounds).collider.bounds.max.y - cameraHeight / 2;
-        float minY = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, _bounds).collider.bounds.min.y + cameraHeight / 2;
-
-        float maxX = Physics2D.Raycast(transform.position, Vector2.right, Mathf.Infinity, _bounds).collider.bounds.max.x - cameraWidth / 2;
-        float minX = Physics2D.Raycast(transform.position, Vector2.left, Mathf.Infinity, _bounds).collider.bounds.min.x + cameraWidth / 2;
-
         if (Time.timeScale > 0)
         {
+            float cameraHeight = _mainCamera.orthographicSize * 2;
+            float cameraWidth = cameraHeight * _mainCamera.aspect;
+
+            float maxY = Physics2D.Raycast(transform.position, Vector2.up, Mathf.Infinity, _bounds).collider.bounds.max.y - cameraHeight / 2;
+            float minY = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, _bounds).collider.bounds.min.y + cameraHeight / 2;
+
+            float maxX = Physics2D.Raycast(transform.position, Vector2.right, Mathf.Infinity, _bounds).collider.bounds.max.x - cameraWidth / 2;
+            float minX = Physics2D.Raycast(transform.position, Vector2.left, Mathf.Infinity, _bounds).collider.bounds.min.x + cameraWidth / 2;
+
             Debug.DrawLine(transform.position, new Vector2(transform.position.x, maxY));
             Debug.DrawLine(transform.position, new Vector2(transform.position.x, minY));
             Debug.DrawLine(transform.position, new Vector2(maxX, transform.position.y));
@@ -44,8 +41,8 @@ public class CameraController : MonoBehaviour
 
     public IEnumerator CameraMoving(bool vert, bool horz)
     {
-        cameraHeight = _mainCamera.orthographicSize * 2;
-        cameraWidth = cameraHeight * _mainCamera.aspect;
+        float cameraHeight = _mainCamera.orthographicSize * 2;
+        float cameraWidth = cameraHeight * _mainCamera.aspect;
 
         float maxY = Physics2D.Raycast(_target.position, Vector2.up, Mathf.Infinity, _bounds).collider.bounds.max.y - cameraHeight / 2;
         float minY = Physics2D.Raycast(_target.position, Vector2.down, Mathf.Infinity, _bounds).collider.bounds.min.y + cameraHeight / 2;
@@ -69,7 +66,7 @@ public class CameraController : MonoBehaviour
                 while (transform.position.x != targetPos.x)
                 {
                     transform.position = Vector3.MoveTowards(transform.position, new Vector3(targetPos.x, transform.position.y, transform.position.z), 1);
-                    yield return new WaitForSecondsRealtime(0.01f);
+                    yield return new WaitForSecondsRealtime(0.001f);
                 }
             }
             if (horz)
@@ -77,7 +74,7 @@ public class CameraController : MonoBehaviour
                 while (transform.position.y != targetPos.y)
                 {
                     transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, targetPos.y, transform.position.z), 1);
-                    yield return new WaitForSecondsRealtime(0.01f);
+                    yield return new WaitForSecondsRealtime(0.001f);
                 }
             }
             transform.position = Vector3.MoveTowards(transform.position, targetPos, 1);
---
+++ Assets/Scripts/PolySnapperEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PolyColliderSnapper))]
+public class PolySnapperEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PolyColliderSnapper Psnapper = (PolyColliderSnapper)target;
+        if (GUILayout.Button("Snap Points"))
+        {
+            Psnapper.Snap();
+        }
+    }
+}
